ed847b7b8e89f1c6233cd8bee74241be28fd3512
Alina <79420032+alina-multi@users.noreply.github.com>
2022-10-31 22:23:27 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,6 @@ export class App extends Component {
     const percentage = this.countPositiveFeedbackPercentage();
     const { good, neutral, bad } = this.state;
     const totalFeedback = this.countTotalFeedback();
-    const percenage = this.countPositiveFeedbackPercentage(); 
 
     return (
       <div>
